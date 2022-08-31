7779a8bb2a40c84f4df5a44b1c37673ead6f1b90
Nishtha Patel <nishthapatel0502@gmail.com>
2022-08-31 13:40:38 +0530

    p2

---
+++ app/src/main/java/com/example/a20012011116_nishtha_prac_2/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.a20012011116_nishtha_prac_2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    val tag="MainActivity"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    private fun showmessage(msg:String)
+    {
+        Log.i(tag,msg)
+        Toast.makeText(this,msg,Toast.LENGTH_LONG).show()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        showmessage("onStart is called....")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        showmessage("onResume is called....")
+    }
+}
